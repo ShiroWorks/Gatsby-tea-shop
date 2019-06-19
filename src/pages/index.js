d1917a74c85459d74b391a0a0b3754cbde30d3b9
Szabo Vilmos <vili8899s@gmail.com>
2019-06-20 00:34:11 +0200

    v1.0.0

---
+++ src/pages/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import BackgroundSection from '../components/Globals/BackgroundSection';
+import Info from '../components/Home/Info';
+import Menu from '../components/Home/Menu';
+import Products from '../components/Home/Products';
+import Contact from '../components/Home/Contact';
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title="tea shop"
+        styleClass="default-background"
+      />
+      <Info />
+      <Menu items={data.menu} />
+      <Products />
+      <Contact />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+
+    menu: allContentfulTeaItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
